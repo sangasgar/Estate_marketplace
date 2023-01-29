dc923c87dc50b97161c64a3be54a248745b988a6
sangasgar <sangas@yandex.ru>
2023-01-29 23:41:14 +0300

    Добавлена авторизация через jwt токен

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { TokenModule } from 'src/token/token.module';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategy';
 
 @Module({
   imports: [UserModule, TokenModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
